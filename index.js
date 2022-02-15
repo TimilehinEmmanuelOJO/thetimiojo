4d01f2d93f4d254e8690b4696a9a1909e2a06725
Timilehin OJO <timilehin175@gmail.com>
2022-02-15 23:01:52 +0100

    this is another commmit

--- index.js
+++ index.js
@@ -5,24 +5,3 @@ function myFunction(){
 
 
 
-<footer>
-<div class="footer">
-    <div class="sm">
-        <h1>timiojo/></h1>
-        <div class="links">
-            <address><a href="mailto:timilehin175@gmail.com"target="_blank"></a></address>
-            <a href="mailto:timilehin175@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
-            <a href="tel:08163393738"><i class="fas fa-phone-square"></i></a>
-            <a href="https://twitter.com/ojo_timi" target="_blank"><i class="fab fa-twitter"></i></a>
-            <a href="https://www.linkedin.com/in/emmanuel-timilehin-ojo-b6ba0170/" target="_blank"><i class="fab fa-linkedin"></i></a>
-            <a href="https://www.instagram.com/ojo_timi/"><i class="fab fa-instagram"></i></a>
-        </div>
-    
-    </div>
-    <div id="foot" class="text-muted">
-        <h1>Projects</h1>
-        <h1>About</h1>
-        <h1>Contact</h1>
-    </div>
-</div>
-</footer>
